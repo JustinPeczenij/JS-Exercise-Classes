a35c3708ed8cb1a53d80d6dafdbc3f26b0706519
Justin Peczenij <justinpeczenij@gmail.com>
2021-02-25 15:32:24 -0500

    task one completed

--- index.js
+++ index.js
@@ -45,9 +45,23 @@ class Airplane {
     constructor(name, age){
       this.name = name;
       this.age = age;
+      this.stomach = []
     }
-  }
-  
+
+    eat(edible){
+      if(this.stomach.length < 10){
+        this.stomach.push(edible)
+      }
+    }
+
+    poop(){
+      this.stomach = []
+    }
+
+    toString(){
+      return `"${this.name}, ${this.age}"`
+    }
+  }  
   /*
     TASK 2
       - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
